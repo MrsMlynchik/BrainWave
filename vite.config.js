1698426229e25556025e6cda3de349f529efe852
Tanya <seize.the.day102306@gmail.com>
2025-05-23 14:40:12 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+=======
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+>>>>>>> 9ae4e79 (Initial commit)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
 })
+=======
+  base: "/BrainWave/",
+});
+>>>>>>> 9ae4e79 (Initial commit)
